6d99236e9d9224e0c005a7d104ec858d058dc1c4
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-24 23:38:45 -0500

    Trying fetch for local file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('Hello world')
+
+// Initialize functions
+const init = () => {
+    getAlphaItems()
+}
+
+// Function definitions
+const getAlphaItems = () => {
+    fetch('./assets/alpha.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+// Load page
+init()
