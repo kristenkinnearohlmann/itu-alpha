268bfd126ae0bcc20cc0f0f97cd79ac031016d22
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-09-02 09:25:27 -0500

    Initial display of all information

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const displayLetter = document.getElementById('display-letter')
+const phoneticWord = document.getElementById('phonetic-word')
+const pronunciation = document.getElementById('pronunciation')
 
 // Initialize functions
 const init = () => {
@@ -17,6 +19,8 @@ const displayInitialAlpha = () => {
     console.log('Display function')
     console.log(alpha[0])
     displayLetter.textContent = alpha[0].letter
+    phoneticWord.textContent = alpha[0].phonetic_word
+    pronunciation.textContent = alpha[0].pronunciation
 }
 
 // Load page
