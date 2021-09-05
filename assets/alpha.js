72d3758cb662997aa67bf09a6a41c9e9f1112b8c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-09-05 07:52:40 -0500

    Added draft class definition and methods

--- assets/alpha.js
+++ assets/alpha.js
@@ -3,33 +3,35 @@ const alpha = [
         "letter": "A",
         "phonetic_word": "Alpha",
         "pronunciation": "al-fah",
-        "stress_syllable": [1]
+        "stress_syllable": [1],
+        "symbol_img_src": ""
     },
     {
         "letter": "B",
         "phonetic_word": "Bravo",
         "pronunciation": "brah-voh",
-        "stress_syllable": [1,2]
+        "stress_syllable": [1,2],
+        "symbol_img_src": ""
     },
     {
         "letter": "C",
         "phonetic_word": "Charlie",
         "pronunciation": "char-lee",
         "stress_syllable": [1],
-        "symbol": ""
+        "symbol_img_src": ""
     },
     {
         "letter": "D",
         "phonetic_word": "Delta",
         "pronunciation": "dell-tah",
         "stress_syllable": [1],
-        "symbol": ""
+        "symbol_img_src": ""
     },
     {
         "letter": "E",
         "phonetic_word": "Echo",
         "pronunciation": "eck-oh",
         "stress_syllable": [1],
-        "symbol": ""
+        "symbol_img_src": ""
     }
 ]
--- src/alphaletter.js
+++ src/alphaletter.js
@@ -1,5 +1,14 @@
 class AlphaLetter {
     constructor(data) {
-        
+        this.letter = data.letter
+    }
+
+    get letter() {
+        return this.letter
+    }
+
+    styleStressSyllables() {
+
+        return stressedWord
     }
 }
