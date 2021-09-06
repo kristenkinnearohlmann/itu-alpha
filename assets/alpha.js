e28cfab372367f4b1f3f7470f848d32e9a7710a2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-09-05 23:44:50 -0500

    Added all alpha letters to set

--- assets/alpha.js
+++ assets/alpha.js
@@ -33,5 +33,152 @@ const alpha = [
         "pronunciation": "eck-oh",
         "stress_syllable": [1],
         "symbol_img_src": ""
+    },
+    {
+        "letter": "F",
+        "phonetic_word": "Foxtrot",
+        "pronunciation": "foks-trot",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "G",
+        "phonetic_word": "Golf",
+        "pronunciation": "golf",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "H",
+        "phonetic_word": "Hotel",
+        "pronunciation": "hoh-tell",
+        "stress_syllable": [2],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "I",
+        "phonetic_word": "India",
+        "pronunciation": "in-dee-ah",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "J",
+        "phonetic_word": "Juliet",
+        "pronunciation": "jew-lee-ett",
+        "stress_syllable": [1,3],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "K",
+        "phonetic_word": "Kilo",
+        "pronunciation": "kee-loh",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "L",
+        "phonetic_word": "Lima",
+        "pronunciation": "lee-mah",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "M",
+        "phonetic_word": "Mike",
+        "pronunciation": "mike",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "N",
+        "phonetic_word": "November",
+        "pronunciation": "noh-vem-ber",
+        "stress_syllable": [2],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "O",
+        "phonetic_word": "Oscar",
+        "pronunciation": "oss-cah",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "P",
+        "phonetic_word": "Papa",
+        "pronunciation": "pah-pah",
+        "stress_syllable": [2],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "Q",
+        "phonetic_word": "Quebec",
+        "pronunciation": "keh-beck",
+        "stress_syllable": [2],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "R",
+        "phonetic_word": "Romeo",
+        "pronunciation": "row-me-oh",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "S",
+        "phonetic_word": "Sierra",
+        "pronunciation": "see-air-ah",
+        "stress_syllable": [2],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "T",
+        "phonetic_word": "Tango",
+        "pronunciation": "tang-go",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "U",
+        "phonetic_word": "Uniform",
+        "pronunciation": "you-nee-form",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "V",
+        "phonetic_word": "Victor",
+        "pronunciation": "vik-tah",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "W",
+        "phonetic_word": "Whiskey",
+        "pronunciation": "wiss-key",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "X",
+        "phonetic_word": "X-ray",
+        "pronunciation": "ecks-ray",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "Y",
+        "phonetic_word": "Yankee",
+        "pronunciation": "yang-key",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
+    },
+    {
+        "letter": "Z",
+        "phonetic_word": "Zulu",
+        "pronunciation": "zoo-loo",
+        "stress_syllable": [1],
+        "symbol_img_src": ""
     }
 ]
