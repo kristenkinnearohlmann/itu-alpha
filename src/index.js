e2bf731f74245ac795086f0b68aecb45369f0188
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-09-08 20:28:07 -0500

    Adjusted image display to ensure all are centered

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const loadAlphaItems = () => {
 const displayInitialAlpha = () => {
     console.log('Display function')
 
-    const currentLetter = new AlphaLetter(alpha[2])
+    const currentLetter = new AlphaLetter(alpha[14])
     console.log(currentLetter)
 
     displayLetter.textContent = currentLetter.letter
