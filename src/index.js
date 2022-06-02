4b444da5f2abc4cb8ad6e9eeb1252a2b0e495324
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-06-01 19:53:17 -0500

    Create and implement input element for letter changes

--- src/index.js
+++ src/index.js
@@ -1,94 +1,118 @@
-const displayLetter = document.getElementById('display-letter')
-const changeLetter = document.getElementById('change-letter')
-const changeLetterInput = document.getElementById('change-letter-input')
-const displayLetterImg = document.getElementById('display-letter-img')
-const phoneticWord = document.getElementById('phonetic-word')
-const pronunciation = document.getElementById('pronunciation')
-const copyYears = document.getElementById('copy-years')
-let alphaPosition = 0
-let currentLetter = ""
+const displayLetter = document.getElementById("display-letter");
+const changeLetter = document.getElementById("change-letter");
+const changeLetterInput = document.getElementById("change-letter-input");
+const letterInputEntry = document.getElementById("letter-input-entry");
+const displayLetterImg = document.getElementById("display-letter-img");
+const phoneticWord = document.getElementById("phonetic-word");
+const pronunciation = document.getElementById("pronunciation");
+const copyYears = document.getElementById("copy-years");
+let alphaPosition = 0;
+let currentLetter = "";
 
 // Initialize functions
 const init = () => {
-    setCopyYears()
-    loadAlphaItems()
-    displayAlpha()
-}
+  setCopyYears();
+  loadAlphaItems();
+  displayAlpha();
+};
 
 // Function definitions
 const setCopyYears = () => {
-    const originalYear = 2021
-    const currentDate = new Date()
-    copyYears.innerText = (originalYear === currentDate.getFullYear() ? `${originalYear}` : `${originalYear} - ${currentDate.getFullYear()}`)
-}
+  const originalYear = 2021;
+  const currentDate = new Date();
+  copyYears.innerText =
+    originalYear === currentDate.getFullYear()
+      ? `${originalYear}`
+      : `${originalYear} - ${currentDate.getFullYear()}`;
+};
 
-const loadAlphaItems = () => { 
-    return alpha
-}
+const loadAlphaItems = () => {
+  return alpha;
+};
 
 const displayAlpha = () => {
+  currentLetter = new AlphaLetter(alpha[alphaPosition]);
 
-    currentLetter = new AlphaLetter(alpha[alphaPosition])
+  //   displayLetter.textContent = currentLetter.letter;
+  //   console.log(currentLetter.letter);
+  //   letterInputEntry.placeholder = currentLetter.letter;
+  letterInputEntry.value = currentLetter.letter;
+  letterInputEntry.blur();
+  displayLetterImg.innerHTML = currentLetter.renderLetterImg();
 
-    displayLetter.textContent = currentLetter.letter
-    displayLetterImg.innerHTML = currentLetter.renderLetterImg()
-    
-    phoneticWord.textContent = currentLetter.phoneticWord
-    pronunciation.innerHTML = currentLetter.stressPronounce()
-
-}
+  phoneticWord.textContent = currentLetter.phoneticWord;
+  pronunciation.innerHTML = currentLetter.stressPronounce();
+};
 
 const changeDisplayLetter = () => {
+  displayLetter.classList.add("hidden");
+  changeLetter.classList.remove("hidden");
 
-    displayLetter.classList.add("hidden")
-    changeLetter.classList.remove("hidden")
+  changeLetterInput.placeholder = currentLetter.letter;
 
-    changeLetterInput.placeholder = currentLetter.letter
-      
-    changeLetterInput.focus()
-    changeLetterInput.select()
-}
+  changeLetterInput.focus();
+  changeLetterInput.select();
+};
 
 const updateDisplayLetter = (event) => {
+  const newLetter = event.target.value.toUpperCase();
+  const newLetterPosition = alpha
+    .map((element) => element.letter)
+    .indexOf(newLetter);
+  alphaPosition = newLetterPosition;
 
-    const newLetter = event.target.value.toUpperCase()
-    const newLetterPosition = alpha.map(element => element.letter).indexOf(newLetter)
-    alphaPosition = newLetterPosition
+  changeLetter.classList.add("hidden");
+  displayLetter.classList.remove("hidden");
+  displayAlpha();
+  changeLetterInput.value = "";
+};
 
-    changeLetter.classList.add("hidden")
-    displayLetter.classList.remove("hidden")
-    displayAlpha()
-    changeLetterInput.value = ""
-}
+document.getElementById("arrow-left").addEventListener("click", () => {
+  alphaPosition === 0 ? (alphaPosition = 25) : alphaPosition--;
+  displayAlpha();
+});
 
-document.getElementById('arrow-left').addEventListener('click', () => {
-    alphaPosition === 0 ? alphaPosition = 25 : alphaPosition--
-    displayAlpha()
-})
+document.getElementById("arrow-right").addEventListener("click", () => {
+  alphaPosition === 25 ? (alphaPosition = 0) : alphaPosition++;
+  displayAlpha();
+});
 
-document.getElementById('arrow-right').addEventListener('click', () => {
-    alphaPosition === 25 ? alphaPosition = 0 : alphaPosition++
-    displayAlpha()
-})
+document.getElementById("display-letter").addEventListener("click", () => {
+  changeDisplayLetter();
+});
 
-document.getElementById('display-letter').addEventListener('click', () => {
-    changeDisplayLetter()
-})
+changeLetterInput.addEventListener("keyup", (event) => {
+  const keyPressed = event.key;
 
-changeLetterInput.addEventListener('keyup', (event) => {
-    const keyPressed = event.key
+  if (keyPressed === "Enter") {
+    updateDisplayLetter(event);
+  }
+});
 
-    if (keyPressed === 'Enter') {
-        updateDisplayLetter(event)
-    }
-})
+changeLetterInput.addEventListener("blur", (event) => {
+  // invoke only if the blur occurs when the input is display
+  if (!changeLetter.classList.contains("hidden")) {
+    updateDisplayLetter(event);
+  }
+});
 
-changeLetterInput.addEventListener('blur',(event) => {
-    // invoke only if the blur occurs when the input is display
-    if (!changeLetter.classList.contains("hidden")) {
-        updateDisplayLetter(event)
-    }
-})
+letterInputEntry.addEventListener("click", (event) => {
+  letterInputEntry.value = "";
+});
+
+letterInputEntry.addEventListener("blur", (event) => {
+  //   window.alert("Blurred new input");
+  updateDisplayLetter(event);
+});
+
+letterInputEntry.addEventListener("keyup", (event) => {
+  const keyPressed = event.key;
+
+  if (keyPressed === "Enter") {
+    // window.alert("Enter keyup new input");
+    updateDisplayLetter(event);
+  }
+});
 
 // Load page
-init()
+init();
